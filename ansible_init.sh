1cd128867e87952a659617b1e43054e7e762d085
Socket-Wrench <johnjgrabowski@gmail.com>
2020-06-25 16:29:51 -0700

    refactored to include git and better checking.

--- ansible_init.sh
+++ ansible_init.sh
@@ -1,19 +1,38 @@
 #!/bin/sh
 #Initial installation for ansible centos8
 
-REPO_ADDR="https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm"
 RHRELEASE=$(cat /etc/redhat-release)
+APPSTREAM_REPO_NAME=AppStream
+EPEL_REPO_ADDR="https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm"
+EPEL_REPO_NAME=epel
 
-rpm -q ansible && echo "Ansible already installed.  Exitting..." && exit 0
 
 if [[ "$RHRELEASE" =~ (^CentOS Linux release 8.*$) ]]
 then
-  rpm -q dnf-plugins-core || sudo dnf install -y dnf-plugins-core || exit 1
-  sudo dnf repolist -y |grep -E "^epel.*$" || sudo dnf install -y $REPO_ADDR 
-  sudo dnf config-manager -y --dump epel|grep 'enabled = 1' || sudo dnf config-manager -y --enable epel
-  sudo dnf install -y python3 python3-pip
-  sudo pip3 install ansible
-  ansible --version
+  rpm -q dnf-plugins-core || \
+    sudo dnf install -y dnf-plugins-core || \
+    exit 1
+  for p in dnf-plugins-core python3 python3-pip git
+  do
+    rpm -q $p || pkg_list="$pkg_list $p"
+  done
+  [ "echo $pkg_list|tr -d '[:blank:]'" ] && \
+    sudo dnf --enablerepo ${APPSTREAM_REPO_NAME} install -y $pkg_list
+  if [ "$(sudo pip3 list --format=columns | grep "^ansible ")" ]
+  then
+    #echo "Ansible already installed.  Exitting..." && exit 0
+    sudo pip3 install ansible
+  elif [ "$(rpm -q ansible)" ]
+  then
+    sudo dnf repolist -y |grep -E "^epel.*$" || \
+      sudo dnf install -y $EPEL_REPO_ADDR 
+    sudo dnf config-manager -y --dump $EPEL_REPO_NAME|grep 'enabled = 1' || \
+      sudo dnf config-manager -y --enable $EPEL_REPO_NAME
+    sudo dnf install -y ansible
+  fi
+  ansible --version || \
+    ( echo "Something didn't work right, review output and try again..." && \
+      exit 1 )
   echo "Installation complete"
   exit 0
 else
