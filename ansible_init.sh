0554b8f83c33e4f0d6cdcb3bb1f7af3e9aea456c
Socket-Wrench <johnjgrabowski@gmail.com>
2020-06-25 17:17:40 -0700

    cleaner syntax and notations

--- ansible_init.sh
+++ ansible_init.sh
@@ -2,43 +2,78 @@
 #Initial installation for ansible centos8
 
 RHRELEASE=$(cat /etc/redhat-release)
+RHEL_EXPECTED='(^CentOS Linux release 8.*$)'
 APPSTREAM_REPO_NAME=AppStream
 EPEL_REPO_ADDR="https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm"
 EPEL_REPO_NAME=epel
+PREREQ_PKGS='dnf-plugins-core python3 python3-pip git'
 
+PIP=$(which pip3 || which pip2 || which pip)
 
-if [[ "$RHRELEASE" =~ (^CentOS Linux release 8.*$) ]]
+function pline(){
+  echo "==============================================================================="
+
+}
+
+# make sure correct os and version or exit
+if ! [[ "$RHRELEASE" =~ $RHEL_EXPECTED ]] 
 then
-  rpm -q dnf-plugins-core || \
-    sudo dnf install -y dnf-plugins-core || \
-    exit 1
-  for p in dnf-plugins-core python3 python3-pip git
-  do
-    rpm -q $p || pkg_list="$pkg_list $p"
-  done
-  [ "echo $pkg_list|tr -d '[:blank:]'" ] && \
-    sudo dnf --enablerepo ${APPSTREAM_REPO_NAME} install -y $pkg_list
-  if [ "$(sudo pip3 list --format=columns | grep "^ansible ")" ]
-  then
+  echo ""
+  pline
+  echo "Wrong operating system or version."
+  echo "Expected: ${RHEL_EXPECTED}"
+  echo "Found: ${RHRELEASE}"
+  echo "Exitting..."
+  exit 1
+fi
+
+# Check to see if prereqs are installed
+echo ""
+pline
+echo "Checking for prerequisites..."
+for p in ${PREREQ_PKGS}
+do
+  rpm -q $p || pkg_list="$pkg_list $p"
+done
+
+# Install prereqs
+if [ "echo $pkg_list|tr -d '[:blank:]'" ]
+then
+  echo ""
+  pline
+  echo "Installing prequisites: ${pkg_list}"
+  sudo dnf --enablerepo ${APPSTREAM_REPO_NAME} install -y $pkg_list
+fi
+
+# Check if ansible is already installed
+if [ "$(ansible --version)" ]
+then
+    ansible --version
+    echo ""
+    echo "Ansible already installed. Nothing else to do.  Exitting..."
+    exit 0
+# Try to install via pip if not already
+elif [ -x ${PIP} ] && [ -z "$(sudo ${PIP} list --format=columns | grep "^ansible ")" ]
+then
     #echo "Ansible already installed.  Exitting..." && exit 0
-    sudo pip3 install ansible
-  elif [ "$(rpm -q ansible)" ]
-  then
+    sudo ${PIP} install ansible
+# Try to install ansible via rpm if not already
+elif [ "$(rpm -q ansible)" ]
+then
     sudo dnf repolist -y |grep -E "^epel.*$" || \
       sudo dnf install -y $EPEL_REPO_ADDR 
     sudo dnf config-manager -y --dump $EPEL_REPO_NAME|grep 'enabled = 1' || \
       sudo dnf config-manager -y --enable $EPEL_REPO_NAME
     sudo dnf install -y ansible
-  fi
-  ansible --version || \
-    ( echo "Something didn't work right, review output and try again..." && \
-      exit 1 )
-  echo "Installation complete"
-  exit 0
-else
-  echo "Unsupported operating system or verison.
-  Expecting: CentOS Linux release 8.x.  
-  Retrieved: $RHRELEASE"
-  exit 1
 fi
 
+# Validate ansible responds or print error and exit
+pline
+echo "Verifying installation...."
+ansible --version || \
+    ( echo "Something didn't work right, review output and try again..." && \
+      exit 1 )
+
+# If you got this far, you should be good, all done, and exit
+echo "Installation complete"
+exit 0
